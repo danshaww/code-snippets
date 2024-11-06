940c855a379d909b526f511994b5d0003af9911e
Dan <danshaw509@gmail.com>
2024-11-06 20:50:36 +0000

    Added additional loop

--- linux/scripting/For Each Loops.sh
+++ linux/scripting/For Each Loops.sh
@@ -2,3 +2,6 @@
 for dir in /docker/*; do (cd "$dir" && docker compose down && docker compose up -d); done
 
 for dir in /docker/apps/*; do (cd "$dir" && docker compose down && docker compose up -d); done
+
+#Loop using output from another command:
+for i in $(docker compose ls -q); do (echo $i); done
