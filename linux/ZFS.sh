f16793734a76203c84242f3d08c0c5a78ae4aa08
Dan <danshaw509@gmail.com>
2024-12-24 13:42:54 +0000

    Updated ZFS

--- linux/ZFS.sh
+++ linux/ZFS.sh
@@ -7,3 +7,11 @@ zpool status
 # List all datasets and volumes on all or a specific ZFS Pool on a system
 zfs list
 
+# Create a new ZFS Dataset
+zfs create {poolname}/{datasetname}
+
+# Get Mountpoint of a ZFS Pool
+zfs get mountpoint backups
+
+# Set new mountpoint for ZFS Pool
+zfs set mountpoint=/mnt/backups backups
